14ea5375f8e03366502dabe8fab66646674ed096
Nipun Chamika <nipunchamika11@gmail.com>
2024-05-17 16:26:25 +0530

    feat: Connect to database using Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,18 +1,40 @@
 import os
-from flask import Flask, render_template, flash
+from datetime import datetime, timezone
 from dotenv import load_dotenv
+from flask import Flask, render_template, flash
+from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, EmailField
 from wtforms.validators import DataRequired
 
 load_dotenv()
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
+db = SQLAlchemy(app)
 
-class Form(FlaskForm):
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    date_added = db.Column(
+        db.DateTime, default=lambda: datetime.now(timezone.utc))
+
+    def __repr__(self) -> str:
+        return f'<Name {self.name}>'
+
+
+class NameForm(FlaskForm):
     name = StringField('Enter your name', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class UserForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
@@ -40,9 +62,27 @@ def special_exception_handler(error):
 @app.route('/name-form', methods=['GET', 'POST'])
 def name_form():
     name = None
-    form = Form()
+    form = NameForm()
     if form.validate_on_submit():
         name = form.name.data
         form.name.data = ''
         flash('Form submitted successfully!')
-    return render_template('form.html', name=name, form=form)
+    return render_template('name_form.html', name=name, form=form)
+
+
+@app.route('/users', methods=['GET', 'POST'])
+def user_form():
+    name = None
+    form = UserForm()
+    if form.validate_on_submit():
+        existing_user = User.query.filter_by(email=form.email.data).first()
+        if existing_user is None:
+            new_user = User(name=form.name.data, email=form.email.data)
+            db.session.add(new_user)
+            db.session.commit()
+        name = form.name.data
+        form.name.data = ''
+        form.email.data = ''
+        flash('User added successfully!')
+    users = User.query.order_by(User.date_added)
+    return render_template('user_form.html', name=name, form=form, users=users)
